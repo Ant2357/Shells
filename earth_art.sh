0dfda0667e204afd5003e33729f1cb2fe4d1cd8b
Ant <ryu4979@gmail.com>
2021-11-08 19:47:24 +0900

    feat: earth_art.sh

---
+++ earth_art.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# set term gif animate delay 13;
+# set output '/images/x.gif';
+# set view equal xyz;
+# set isosamples 20;
+# set parametric;
+
+# do for [i=0:365] {
+#   splot sin(u+i)*cos(v), sin(u+i)*sin(v), cos(u+i);
+# }
+
+gnuplot -e "set term gif animate delay 13; set output '/images/x.gif'; set view equal xyz; set isosamples 20; set parametric; do for [i=0:365] { splot sin(u+i)*cos(v), sin(u+i)*sin(v), cos(u+i); }"
