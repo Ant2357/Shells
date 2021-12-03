b1b2079dbdb45be7e01069953c87d58d900282a0
Ant <ryu4979@gmail.com>
2021-12-04 00:46:26 +0900

    fix: タイプミス修正(earth_art.sh)

--- earth_art.sh
+++ earth_art.sh
@@ -6,8 +6,8 @@
 # set isosamples 20;
 # set parametric;
 
-# do for [i=0:365] {
+# do for [i=0:360] {
 #   splot sin(u+i)*cos(v), sin(u+i)*sin(v), cos(u+i);
 # }
 
-gnuplot -e "set term gif animate delay 13; set output '/images/x.gif'; set view equal xyz; set isosamples 20; set parametric; do for [i=0:365] { splot sin(u+i)*cos(v), sin(u+i)*sin(v), cos(u+i); }"
+gnuplot -e "set term gif animate delay 13; set output '/images/x.gif'; set view equal xyz; set isosamples 20; set parametric; do for [i=0:360] { splot sin(u+i)*cos(v), sin(u+i)*sin(v), cos(u+i); }"
